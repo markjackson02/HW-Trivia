337d089eea4d6b1159527c89d8f18b73651ba45a
Mark Jackson <35121685+markjackson02@users.noreply.github.com>
2018-06-17 15:36:44 -0400

    layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+$("#start-btn").on("click", function(){
+    // $("#question-screen").toggleClass("d-none");
+    console.log("test");
+});
+
+// document.getElementById("start-btn").addEventListener("click", function() {
+//     console.log("test");
+// });
---
+++ assets/javascript/questions.js
@@ -0,0 +1,19 @@
+var questions = {
+    // Correct answer is the first one in the options array
+    1: {
+        question: "The earliest archeological evidence of grape wine dates back to",
+        options: ["6000 B.C.", "600 B.C.", "60000 B.C.", "600000 B.C."]
+    },
+    2: {
+        question: "In the United States, for a wine to be vintage-dated and labeled with a country/location of origin, __% of its volume must be from grapes harvested in that year",
+        options: ["95%", "51%", "75%", "100%"]
+    },
+    3: {
+        question: "Burgundy wine is only made in:",
+        options: ["France", "Italy", "United States", "North Pole"]
+    },
+    4: {
+        question: "Sake is a type of wine made from:",
+        options: ["Rice", "Potatoes", "Wheat", "Gasoline"]
+    }
+}
