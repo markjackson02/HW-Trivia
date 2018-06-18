2f900a9a6cdf44f03b5114211d736bb63ab7ed99
Mark Jackson <35121685+markjackson02@users.noreply.github.com>
2018-06-17 22:57:53 -0400

    progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,73 @@
 $(document).ready(function() {
-    $("#start-btn").click(function(event){
-        // $("#question-screen").toggleClass("d-none");
-        console.log("test");
+
+    var currentQuestion = -1;
+    var currentAnswer;
+    var numCorrect = 0;
+    var numIncorrect = 0;
+    var numUnanswered = 0;
+
+    // function reset() {
+    //     currentQuestion = -1;
+    //     userAnswers = [];
+    // ... etc
+        
+    // }
+    
+    $("#start-btn").click(function(){
+        $("#game-start-screen").addClass("d-none");
+        $("#question-screen").removeClass("d-none");
+        nextQuestion();
     });
+
+    $(".possible-answers").click(function() {
+        $("#next-question-btn").removeClass("d-none"); // temporary until timers implemented
+        currentAnswer = $(this).text();
+        questionResult();
+    });
+
+    $("#next-question-btn").click(function() { // temporary until timers implemented
+        $("#next-question-btn").addClass("d-none");
+        nextQuestion();
+    })
+
+    function nextQuestion() {
+        //reset green/red success/danger buttons
+        $(".possible-asnswers").each(function () {
+            $(this).removeClass("btn-danger");
+            $(this).removeClass("btn-success");
+        });
+        
+        currentQuestion++;
+        if (currentQuestion < questions.length) {
+            $(".current-question").text(questions[currentQuestion].question);
+            $(".possible-answers").each(function(index) {
+                $(this).text(questions[currentQuestion].options[index]);
+            });
+        } else {
+            gameResults();
+        }
+    };
+
+    function questionResult() {
+        $(".possible-answers").each(function(index) {
+            if ($(this).text === questions[currentQuestion].correctAnswer) { //make correct answer green
+                $(this).addClass("btn-success");
+                if ($(this).text === currentAnswer) {
+                    numCorrect++;
+                }
+            } else {
+                numIncorrect++; // once introduct timers, need to account for unanswered
+                if ($(this).text === currentAnswer) { // if user guessed incorrect answer make that one red
+                    $(this).addClass("btn-danger");
+                }
+            }
+            
+        });
+    }
+
+    function gameResults() {
+        $("#question-screen").addClass("d-none");
+        $("#game-results").removeClass("d-none");
+    };
 });
 
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,19 +1,39 @@
-var questions = {
+var questions = shuffle([
     // Correct answer is the first one in the options array
-    1: {
+    {
         question: "The earliest archeological evidence of grape wine dates back to",
-        options: ["6000 B.C.", "600 B.C.", "60000 B.C.", "600000 B.C."]
+        correctAnswer: "6000 B.C.",
+        options: shuffle(["6000 B.C.", "600 B.C.", "60000 B.C.", "600000 B.C."])
     },
-    2: {
+    {
         question: "In the United States, for a wine to be vintage-dated and labeled with a country/location of origin, __% of its volume must be from grapes harvested in that year",
-        options: ["95%", "51%", "75%", "100%"]
+        correctAnswer: "95%",
+        options: shuffle(["95%", "51%", "75%", "100%"])
     },
-    3: {
+    {
         question: "Burgundy wine is only made in:",
-        options: ["France", "Italy", "United States", "North Pole"]
+        correctAnswer: "France",
+        options: shuffle(["France", "Italy", "United States", "North Pole"])
     },
-    4: {
+    {
         question: "Sake is a type of wine made from:",
-        options: ["Rice", "Potatoes", "Wheat", "Gasoline"]
+        correctAnswer: "Rice",
+        options: shuffle(["Rice", "Potatoes", "Wheat", "Gasoline"])
     }
+]);
+
+function shuffle(array) { // randomizes order
+    let counter = array.length;
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+        // Decrease counter by 1
+        counter--;
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
 }
