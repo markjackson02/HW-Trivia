bd8b07588868e52b63f37301403a7e84e3772a66
Mark Jackson <35121685+markjackson02@users.noreply.github.com>
2018-06-20 21:32:46 -0400

    getting there... question timer working, need to add another timer for answer screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,59 +5,64 @@ var numQuestions = questions.length;
 var currentQuestion = -1;
 var startingTime = 5; // seconds
 var timeLeft = 5; // seconds
+var intervalID = null;
 var currentAnswer;
 var numCorrect = 0;
 var numIncorrect = 0;
 var numUnanswered = 0;
 var finalScore;
 
+function toStartScreen() {
+    $("#game-start-screen").addClass("d-none");
+    $("#question-screen").removeClass("d-none");
+};
+
 $(document).ready(function() {
 
-    // function reset() {
-    //     currentQuestion = -1;
-    //     userAnswers = [];
-    // ... etc
-        
-    // }
-
-    //Timers
-    $("#starting-time").text(startingTime);
-    var countdownTimer = setInterval(updateTime, 1000);
+    // Timer
+    var countdownTimer;
     function updateTime() {
         timeLeft--;
         $("#countdown-timer").text(timeLeft);
         if (timeLeft <= 0) {
             clearInterval(countdownTimer);
-            console.log("Time up!");
         }
-    }
+    };
 
+    // Set home screen values
+    $("#starting-time").text(startingTime);
     $("#num-questions").text(numQuestions);
     
     $("#start-btn").click(function(){
-        $("#game-start-screen").addClass("d-none");
-        $("#question-screen").removeClass("d-none");
+        toStartScreen();
         nextQuestion();
     });
 
     $(".possible-answers").click(function() {
-        clearInterval(countdownTimer);
+
+        // Timer
+        clearInterval(countdownTimer); // after answer is clicked
+
         $("#countdown-timer-div").addClass("d-none");
         $("#next-question-btn").removeClass("d-none"); // TO-DO: temporary until timers implemented
         currentAnswer = $(this).text();
-        questionResult();
+        toQuestionResult();
         // TO-DO: need to make buttons unclickable on question result screen
     });
 
     $("#next-question-btn").click(function() { // TO-DO: temporary until timers implemented
         $("#next-question-btn").addClass("d-none");
         nextQuestion();
-    })
+    });
 
     function nextQuestion() {
 
         $("#countdown-timer-div").removeClass("d-none");
         timeLeft = startingTime;
+        console.log("time left at nextQuestion(): " + timeLeft);
+
+        // Timer
+        countdownTimer = setInterval(updateTime, 1000);
 
         // reset button colors
         $(".possible-answers").each(function () {
@@ -76,34 +81,39 @@ $(document).ready(function() {
                 $(this).text(questions[currentQuestion].options[index]);
             });
         } else {
-            gameResults();
+            toGameResults();
         }
     };
 
-    function questionResult() {
-            if (currentAnswer === questions[currentQuestion].correctAnswer) {
-                numCorrect++;
-            } else if (currentAnswer === null) {
-                numUnanswered++;
-            } else {
-                numIncorrect++;
-            }
+    function toQuestionResult() {
+        
+        // Increment counters to show on results page
+        if (currentAnswer === questions[currentQuestion].correctAnswer) {
+            numCorrect++;
+        } else if (currentAnswer === null) {
+            numUnanswered++;
+        } else {
+            numIncorrect++;
+        }
 
-            $(".possible-answers").each(function(index) {
-                if ($(this).text() === questions[currentQuestion].correctAnswer) {
-                    $(this).addClass("btn-success");
-                } else if ($(this).text() === currentAnswer) {
-                    $(this).addClass("btn-danger");
-                }
-            });
-            var imgURL = "assets/images/" + questions[currentQuestion].answerImage;
-            console.log(imgURL);
-            $("#answer-image").attr("src", imgURL);
-            $("#answer-text").text(questions[currentQuestion].answerText);
-            $("#question-result").removeClass("d-none");
+        // Change button colors to: Green for Correct Answer, Red for Incorrectly guessed answer
+        $(".possible-answers").each(function() {
+            if ($(this).text() === questions[currentQuestion].correctAnswer) {
+                $(this).addClass("btn-success");
+            } else if ($(this).text() === currentAnswer) {
+                $(this).addClass("btn-danger");
+            }
+        });
+
+        // Show Answer Information
+        var imgURL = "assets/images/" + questions[currentQuestion].answerImage;
+        console.log(imgURL);
+        $("#answer-image").attr("src", imgURL);
+        $("#answer-text").text(questions[currentQuestion].answerText);
+        $("#question-result").removeClass("d-none");
     };
 
-    function gameResults() {
+    function toGameResults() {
         $("#question-screen").addClass("d-none");
         finalScore = Math.floor((numCorrect / (numIncorrect + numUnanswered + numCorrect))*100);
         $("#final-score").text(finalScore);
